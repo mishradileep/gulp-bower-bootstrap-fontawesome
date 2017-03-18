272329c01db8be93bfea4a56e41d9244fbb1f86f
Dileep Mishra <mishradileep@gmail.com>
2017-03-18 22:21:45 +0530

    Modify gulp file and add custom fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,59 @@
-var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
+
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass')
+    notify = require("gulp-notify")
     bower = require('gulp-bower');
 
+// Configs.
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
+  sassPath: './resources/sass',
+  bowerDir: './bower_components',
+  customFonts: './custom_fonts'
 }
 
-gulp.task('bower', function() { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+// Bower.
+gulp.task('bower', function() {
+  return bower().pipe(gulp.dest(config.bowerDir))
 });
 
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
+// FontAwesome.
+gulp.task('icons', function() {
+  return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+    .pipe(gulp.dest('./assets/fonts'));
 });
 
-gulp.task('css', function() { 
-    return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-            .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+// Custom fonts.
+gulp.task('fonts', function() {
+  return gulp.src(config.customFonts + '/**.*')
+    .pipe(gulp.dest('./assets/fonts'));
 });
+
+// jQuery.
+gulp.task('jquery', function() {
+  return gulp.src(config.bowerDir + '/jquery/dist/*.min.js')
+  .pipe(gulp.dest('./assets/vendor'));
+});
+
+// CSS.
+gulp.task('css', function() {
+  return gulp.src(config.sassPath + '/**/*.scss')
+    .pipe(sass({
+    style: 'compressed',
+    loadPath: [
+      './resources/sass',
+      config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+      config.bowerDir + '/fontawesome/scss',
+    ]
+  })
+  .on("error", notify.onError(function (error) {
+    return "Error: " + error.message;
+  })))
+  .pipe(gulp.dest('./assets/css'));
+ });
 
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+gulp.task('watch', function() {
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-  gulp.task('default', ['bower', 'icons', 'css']);
+gulp.task('default', ['bower', 'icons', 'fonts', 'jquery', 'css']);
